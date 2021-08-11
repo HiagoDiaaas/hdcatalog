925751cf1ed125e574619fdfe0a74a12c46a78f2
Hiago <hiagosmd@hotmail.com>
2021-08-10 21:36:54 -0300

    Backend project created

---
+++ backend/src/main/java/com/hiagodias/hdcatalog/HdcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.hiagodias.hdcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdcatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hiagodias/hdcatalog/HdcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiagodias.hdcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

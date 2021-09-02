fd4a2faee146228a1e6b8711bef5e6d90afcd90c
Hiago Santos Martins Dias <hiagosmd@hotmail.com>
2021-09-01 21:31:03 -0300

    Update catgory

--- backend/src/main/java/com/hiagodias/hdcatalog/services/CategoryService.java
+++ backend/src/main/java/com/hiagodias/hdcatalog/services/CategoryService.java
@@ -1,9 +1,10 @@
 package com.hiagodias.hdcatalog.services;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.hiagodias.hdcatalog.dto.CategoryDTO;
 import com.hiagodias.hdcatalog.entities.Category;
 import com.hiagodias.hdcatalog.repositories.CategoryRepository;
-import com.hiagodias.hdcatalog.services.exceptions.EntityNotFoundException;
+import com.hiagodias.hdcatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -33,16 +34,31 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
-		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new CategoryDTO(entity);
 	}
 	
-	@Transactional(readOnly = true)
+	@Transactional
 	public CategoryDTO insert(CategoryDTO dto) {
 		Category entity = new Category();
 		entity.setName(dto.getName());
 		entity = repository.save(entity);
 		return new CategoryDTO(entity);
+	}
+	
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+		Category entity = repository.getOne(id);
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
+	}
+	catch(EntityNotFoundException e) {
+		 throw new ResourceNotFoundException("Id not found " + id);
+			
+		}
+		
 	}
 
 }
